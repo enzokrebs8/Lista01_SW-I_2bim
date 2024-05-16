16e361af4b6c4543003d27507a2083ca8905fdbc
Enzo Krebs Silva <124790985+enzokrebs8@users.noreply.github.com>
2024-05-16 20:29:30 -0300

    Add files via upload

---
+++ exe5/exe5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace exe5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorDeTarefas gerenciador = new GerenciadorDeTarefas();
+
+            // Exemplo de uso
+            gerenciador.AdicionarTarefa("Estudar para a prova", new DateTime(2024, 5, 13));
+            gerenciador.AdicionarTarefa("Ir ao médico", new DateTime(2024, 5, 14));
+            gerenciador.AdicionarTarefa("Enviar atividade", new DateTime(2024, 5, 17));
+
+            gerenciador.ListarTarefas();
+            gerenciador.ListarTarefasHoje();
+
+            gerenciador.RemoverTarefa("Estudar para o exame");
+            gerenciador.ListarTarefas();
+        }
+    }
+}
