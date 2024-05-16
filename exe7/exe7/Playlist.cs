ac0e2e4cc0fe097da1af88161ede28109fdb2e44
Enzo Krebs Silva <124790985+enzokrebs8@users.noreply.github.com>
2024-05-16 20:30:50 -0300

    Add files via upload

---
+++ exe7/exe7/Playlist.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe7
+{
+    class Playlist
+    {
+        private List<Música> músicas;
+        public string Dono { get; }
+
+        public Playlist(string dono)
+        {
+            Dono = dono;
+            músicas = new List<Música>();
+        }
+
+        public void AdicionarMúsica(Música música)
+        {
+            músicas.Add(música);
+        }
+
+        public void TocarMúsicaAleatória()
+        {
+            if (músicas.Count == 0)
+            {
+                Console.WriteLine("A playlist está vazia. Adicione músicas antes de tentar reproduzir.");
+                return;
+            }
+
+            Random random = new Random();
+            int índiceAleatório = random.Next(0, músicas.Count);
+            Música músicaAleatória = músicas[índiceAleatório];
+            Console.WriteLine($"Tocando música aleatória: {músicaAleatória.Nome}");
+        }
+    }
+}
