8e485456622019158d60201f1eb475238a6632ff
Enzo Krebs Silva <124790985+enzokrebs8@users.noreply.github.com>
2024-05-16 20:43:30 -0300

    Add files via upload

---
+++ exe11/exe11/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace exe11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro meuCarro = new Carro("Fusca", 1985);
+
+            // Acelerando o carro
+            meuCarro.Acelerar();
+            meuCarro.Acelerar();
+
+            // Freando o carro
+            meuCarro.Frear();
+            meuCarro.Frear();
+            meuCarro.Frear(); // Freando mais uma vez para garantir que a velocidade não seja negativa
+        }
+    }
+}
